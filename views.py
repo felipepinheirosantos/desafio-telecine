6341cf01f43c3068791f79472db51d9a51ad7f16
Felipe Pinheiro <felipe.pinheiro.dev@gmail.com>
2019-04-09 18:20:57 -0300

    Method to check credentials and provides access token

--- views.py
+++ views.py
@@ -1,12 +1,33 @@
-from flask import jsonify, request
+from flask import jsonify, request, make_response
 from app import app, db
 from werkzeug.security import generate_password_hash, check_password_hash
-import uuid
+import uuid, jwt, datetime
 from models import User
 
 @app.route('/')
 def home():
     return "Desafio Telecine"
+
+
+#Receive user login and pass to generate a token to authorize access
+@app.route('/authenticate')
+def login():
+	auth = request.authorization
+
+	if not auth or not auth.username or not auth.password:
+		return make_response('Could not verify the access data ', 401, { 'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+	user = User.query.filter_by(name=auth.username).first()
+
+	if not user:
+		return make_response('Could not verify the access data', 401, { 'WWW-Authenticate' : 'Basic realm="Login required!"'})
+	
+	if check_password_hash(user.password, auth.password):
+		
+		token = jwt.encode({ 'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+		return jsonify({ 'token' : token.decode('UTF-8') })
+
+	return make_response('Could not verify the access data', 401, { 'WWW-Authenticate' : 'Basic realm="Login required!"'})
 
 
 #create new users
